32796595fda5e259d6d5fd30f2551941107ef5d7
ianArlen <ian.street.fut@gmail.com>
2020-08-12 19:32:26 -0500

    API REST

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const test = require('./routes/test');
+const bodyParser = require('body-parser');
 //const user = require('./routes/user);
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json({extended:false}));
 
 
+let usuario = {
+  name:'',
+  password: ''
+ };
+
+ let respuesta = {
+  error: false,
+  codigo: 200,
+  mensaje: ''
+ };
 //get obtener un usuario
 //get obtener todos los usuarios
 //post guardar un usuario
 //put actualizar informacion de un usuario
 //delete de borrar un usuaio
 
-app.use(express.json({extended:false}));
-
-
 app.use('/pruebita',test);
 
 app.get('/', async (req,res) => {
-  res.send('Holiwi bebe')
+  respuesta = {
+    error: true,
+    codigo: 200,
+    mensaje: 'Punto de inicio'
+   };
+   res.send(respuesta);
 })
 
 app.get('/test',async (req,res) => {
@@ -25,6 +41,122 @@ app.get('/test',async (req,res) => {
     mensaje2: 'aver tu senos bebe'
   })
 })
+
+app.route('/usuario')
+.get(async (req,res) => {
+  respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: ''
+   };
+   if(usuario.name === '' || usuario.password === '') {
+    respuesta = {
+     error: true,
+     codigo: 501,
+     mensaje: 'El usuario no ha sido creado'
+    };
+   } else {
+    respuesta = {
+     error: false,
+     codigo: 200,
+     mensaje: 'respuesta del usuario',
+     respuesta: usuario
+    };
+   }
+   res.send(respuesta);
+})
+
+.post(async (req,res) => {
+  if(!req.body.name || !req.body.password) {
+    respuesta = {
+     error: true,
+     codigo: 502,
+     mensaje: 'El campo name y password son requeridos'
+    };
+   } else {
+    if(usuario.name !== '' || usuario.password !== '') {
+     respuesta = {
+      error: true,
+      codigo: 503,
+      mensaje: 'El usuario ya fue creado previamente'
+     };
+    } else {
+     usuario = {
+      name: req.body.name,
+      password: req.body.password
+     };
+     respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: 'Usuario creado',
+      respuesta: usuario
+     };
+    }
+   }
+   
+   res.send(respuesta);
+})
+
+.put(async (req,res) => {
+  if(!req.body.name || !req.body.password) {
+    respuesta = {
+     error: true,
+     codigo: 502,
+     mensaje: 'El campo name y password son requeridos'
+    };
+   } else {
+    if(usuario.name === '' || usuario.password === '') {
+     respuesta = {
+      error: true,
+      codigo: 501,
+      mensaje: 'El usuario no ha sido creado'
+     };
+    } else {
+     usuario = {
+      name: req.body.name,
+      password: req.body.password
+     };
+     respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: 'Usuario actualizado',
+      respuesta: usuario
+     };
+    }
+   }
+   
+   res.send(respuesta);
+})
+
+.delete(async (req,res) => {
+  if(usuario.name === '' || usuario.password === '') {
+    respuesta = {
+     error: true,
+     codigo: 501,
+     mensaje: 'El usuario no ha sido creado'
+    };
+   } else {
+    respuesta = {
+     error: false,
+     codigo: 200,
+     mensaje: 'Usuario eliminado'
+    };
+    usuario = { 
+     name: '', 
+     password: '' 
+    };
+   }
+   res.send(respuesta);
+});
+
+app.use(async (req,res, next) => {
+  respuesta = {
+    error: true, 
+    codigo: 404, 
+    mensaje: 'URL no encontrada'
+   };
+   res.status(404).send(respuesta);
+});
 
 /*
 const isNumber = (num) => {
@@ -78,4 +210,4 @@ app.post('/sumita',async (req,res) => {
   return res.status(200).json({sumita: sumita})
 });
 
-app.listen(3001, () => console.log(`app listening at http://localhost:${3001}`));
+app.listen(3000, () => console.log(`app listening at http://localhost:${3000}`));
